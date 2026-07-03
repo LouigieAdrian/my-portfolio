d4eb7acad5a5aac64068d82a25f919bebcd040c2
LouigieAdrian <03louigieadrian@gmail.com>
2026-07-03 12:05:10 +0400

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,23 @@ localStorage.setItem("theme", "dark");
 }
 
 });
+
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(entries => {
+
+entries.forEach(entry => {
+
+if(entry.isIntersecting){
+entry.target.classList.add("show");
+}
+
+});
+
+}, {
+threshold: 0.15
+});
+
+sections.forEach(section => {
+observer.observe(section);
+});
