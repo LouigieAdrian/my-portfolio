51b9af6cd7ef3c7917cebc0621543536de059fba
LouigieAdrian <03louigieadrian@gmail.com>
2026-07-03 10:07:39 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Portfolio Loaded");
+const cards=document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+
+card.addEventListener("mouseover",()=>{
+
+card.style.cursor="pointer";
+
+});
+
+});
