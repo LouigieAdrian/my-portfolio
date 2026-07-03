0853344c7191052a9b83c09e2546738f45b0678b
LouigieAdrian <03louigieadrian@gmail.com>
2026-07-03 16:12:10 +0400

    Update script.js

--- script.js
+++ script.js
@@ -116,3 +116,53 @@ navMenu.classList.remove("active");
 });
 
 });
+
+const roles = [
+
+"IT Support Specialist",
+
+"Network Engineer",
+
+"Power BI Developer",
+
+"System Administrator",
+
+"Technical Support Engineer"
+
+];
+
+const typing = document.getElementById("typing");
+
+let roleIndex = 0;
+let charIndex = 0;
+let deleting = false;
+
+function typeEffect(){
+
+const current = roles[roleIndex];
+
+if(!deleting){
+
+typing.textContent = current.substring(0,charIndex++);
+if(charIndex > current.length){
+deleting = true;
+setTimeout(typeEffect,1200);
+return;
+}
+
+}else{
+
+typing.textContent = current.substring(0,charIndex--);
+
+if(charIndex < 0){
+deleting = false;
+roleIndex = (roleIndex + 1) % roles.length;
+}
+
+}
+
+setTimeout(typeEffect,deleting ? 45 : 90);
+
+}
+
+typeEffect();
